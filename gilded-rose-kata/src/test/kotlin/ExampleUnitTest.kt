83e99157b055dd9476e5b1ab446f2a1a8fef159f
Marcello Galhardo <marcello.galhardo@gmail.com>
2021-05-08 21:39:06 +0200

    Add ExampleUnitTest

---
+++ gilded-rose-kata/src/test/kotlin/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ */
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
