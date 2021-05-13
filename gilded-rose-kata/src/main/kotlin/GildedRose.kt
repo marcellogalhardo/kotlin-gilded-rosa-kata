24eeb25f37834b0f034e5b48285812a3844da0f6
Tiago Dávila <tiago.davila@mindera.com>
2021-05-13 19:13:08 +0100

    Test passing on TDD refactoring

--- gilded-rose-kata/src/main/kotlin/GildedRose.kt
+++ gilded-rose-kata/src/main/kotlin/GildedRose.kt
@@ -1,61 +1,45 @@
-class GildedRose(
-        val items: List<Item>) {
-
+class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
-        for (i in 0..items.size-1) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            }
-            else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+        items.forEach { item ->
+            if (item.name == "Sulfuras, Hand of Ragnaros") return
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
+            item.sellIn--
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
+            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.sellIn < 0) {
+                    item.quality = 0
+                    return
                 }
+
+                if (item.sellIn < 5) {
+                    item.quality++
+                }
+
+                if (item.sellIn < 10) {
+                    item.quality++
+                }
+
+                item.quality++
+
+                return
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    }
-                    else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
+            if (item.name == "Aged Brie") {
+                if (item.quality == 50) return
+                if (item.sellIn < 0) {
+                    item.quality++
                 }
-                else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                item.quality++
+            } else if (item.quality > 0) {
+                if (item.sellIn < 0) {
+                    item.quality--
+                    if (item.name == "Conjured Mana Cake") item.quality--
                 }
+                item.quality--
+                if (item.name == "Conjured Mana Cake") item.quality--
             }
         }
-
     }
 
 }
---
+++ gilded-rose-kata/src/test/kotlin/GildedRoseTest.kt
@@ -0,0 +1,234 @@
+import org.junit.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GildedRoseTest {
+
+    @Test
+    fun `at the end of each day our system lowers sellIn`() {
+        val item = Item("Item", 0, 0)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = -1, actual = actualItem.sellIn)
+    }
+
+    @Test
+    fun `at the end of each day our system lowers quality`() {
+        val item = Item("Item", 1, 1)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 0, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `quality may not be lower than 0`() {
+        val item = Item("Item", 0, 0)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 0, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `once the sellIn date has passed, Quality degrades twice as fast`() {
+        val item = Item("Item", 0, 2)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 0, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `the Quality of an item is never negative`() {
+        val item = Item("Item", 1, 0)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 0, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Aged Brie actually increases in Quality the older it gets`() {
+        val item = Item("Aged Brie", 1, 0)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 1, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `once the sellIn date has passed, Aged Brie increases in Quality twice as fast`() {
+        val item = Item("Aged Brie", 0, 0)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 2, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `the Quality of Aged Brie is never more than 50`() {
+        val item = Item("Aged Brie", 1, 50)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 50, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Sulfuras, being a legendary item, never has to be sold`() {
+        val item = Item("Sulfuras, Hand of Ragnaros", 1, 1)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 1, actual = actualItem.sellIn)
+    }
+
+    @Test
+    fun `Sulfuras, being a legendary item, never decreases in Quality`() {
+        val item = Item("Sulfuras, Hand of Ragnaros", 1, 1)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 1, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Backstage increases in Quality by 1 when there are at least 11 days before the concert`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 11, 1)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 2, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Backstage increases in Quality by 2 when there are 10 days before the concert`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 1)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 3, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Backstage increases in Quality by 2 when there are at least 6 days before the concert`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 6, 1)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 3, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Backstage increases in Quality by 3 when there are 5 days before the concert`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 5, 1)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 4, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Backstage increases in Quality by 3 when there are at least 1 day before the concert`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 1, 1)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 4, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Backstage drops to 0 after the concert`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 1)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 0, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Backstage quality stays in 0 after the concert`() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 0, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Conjured items degrade in Quality twice as fast as normal items`() {
+        val item = Item("Conjured Mana Cake", 2, 5)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 3, actual = actualItem.quality)
+    }
+
+    @Test
+    fun `Conjured items degrade in Quality twice as fast as normal items when sellIn is 0`() {
+        val item = Item("Conjured Mana Cake", 0, 5)
+
+        val sut = GildedRose(listOf(item))
+        sut.updateQuality()
+
+        val actualItem = sut.items.first()
+
+        assertEquals(expected = 1, actual = actualItem.quality)
+    }
+}
